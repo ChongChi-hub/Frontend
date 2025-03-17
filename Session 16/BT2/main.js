4c71d96c8d0a287fe900c6a45ac5b03da5229608
Trọng Trí <daotrongtri21092006@gmail.com>
2025-03-17 10:16:13 +0700

    Update main.js

--- Session 16/BT2/main.js
+++ Session 16/BT2/main.js
@@ -2,4 +2,4 @@ let number = 10;
 let price = 20.5;
 let status = true;
 let city = "Ha Noi";
-document.writeln(number, price, status, city)
+document.writeln(number, "\n", price,"\n", staTus,"\n", city);
