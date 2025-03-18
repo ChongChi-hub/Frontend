d4b706d4cafb650345ae6169e7439d4b2f4b9bbd
Trọng Trí <daotrongtri21092006@gmail.com>
2025-03-18 08:28:04 +0700

    Update main.js

--- Session 17/BT8/main.js
+++ Session 17/BT8/main.js
@@ -2,6 +2,6 @@ let startDate = new Date(prompt("Xin mời nhập ngày tháng năm bắt đầu
 let endDate = new Date(prompt("Xin mời nhập ngày tháng năm kết thúc: "));
 let time = endDate - startDate;
 let date = time / (1000 * 60 * 60 * 24);
-document.writeln("Độ lệch là: ", date, " ngày");
+document.writeln("Độ lệch là: ", Math.abs(date), " ngày");
 
 
